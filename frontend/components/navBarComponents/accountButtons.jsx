acdc959df2afd3469a5462e1e46aec5f5434ebe7
Jingming Chen <chenjm8683@gmail.com>
2015-12-15 22:39:02 -0800

    add account drop-down menu and logout button/function

--- frontend/components/navBarComponents/accountButtons.jsx
+++ frontend/components/navBarComponents/accountButtons.jsx
@@ -1,13 +1,36 @@
 var React = require('react');
+var ApiUtil = require('../../util/apiUtil.js');
+
 
 var AccountButtons = React.createClass({
+
+  handleLogOut: function() {
+    ApiUtil.destroySession();
+  },
+
   render: function() {
     return (
-      <ul className="nav navbar-nav navbar-right">
-        <li>
-          <a>
+      <ul className='nav navbar-nav navbar-right'>
+        <li className='dropdown'>
+          <a href='#'
+             className='dropdown-toggle'
+             data-toggle='dropdown'
+             role='button'
+             aria-haspopup='true'
+             aria-expanded='false'
+             >
             {this.props.currentUser.username}
+            <span className='caret'></span>
           </a>
+          <ul className='dropdown-menu'>
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" className="divider"></li>
+            <li onClick={this.handleLogOut}><a href="#">
+              <span className="glyphicon glyphicon-log-in" /> Log Out
+            </a></li>
+          </ul>
         </li>
       </ul>
     );
